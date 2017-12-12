6d2823de740f86843e58c09368558ce49f2dd6c4
João Louvem <Joo Louvem>
2017-12-12 21:45:33 -0200

    no message

---
+++ JohnAppMaker/app/Http/Controllers/TasksController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TasksController extends Controller {
+	public function index() {
+		$user = Auth::user();
+		return view ( 'welcome', compact ( 'user' ) );
+	}
+	
+	public function add() {
+		return view( 'add' );
+	}
+	
+	public function create(Request $request) {
+		$task = new Task();
+		$task->description = $request->description;
+		$task->user_id = Auth::id ();
+		$task->save();
+		return redirect ( '/' );
+	}
+	
+	public function edit(Task $task) {
+		if (Auth::check () && Auth::user ()->id == $task->user_id) {
+			return view ( 'edit', compact ( 'task' ) );
+		} else {
+			return redirect ( '/' );
+		}
+	}
+	
+	public function update(Request $request, Task $task) {
+		if (isset ( $_POST ['delete'] )) {
+			$task->delete ();
+			return redirect ( '/' );
+		} else {
+			$task->description = $request->description;
+			$task->save ();
+			return redirect ( '/' );
+		}
+	}
+}
